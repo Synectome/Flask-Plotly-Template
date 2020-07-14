49cfe9e9b72fc09c473409e6eaee8234cfd363fe
Synectome <jessedgb@gmail.com>
2020-07-13 20:41:13 -0400

    again

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+app_dir = os.path.join(basedir, 'app')
 
 
 class Config(object):
@@ -7,4 +8,5 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    UPLOAD_FOLDER = os.path.join(app_dir, 'uploads_temp')
+    UPLOAD_MAX_SIZE = 5e+8
