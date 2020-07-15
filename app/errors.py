b45c2345e42ed8a0cfece03d577e7d73cb3439f7
Synectome <jessedgb@gmail.com>
2020-07-15 14:54:22 -0400

    403 error code

--- app/errors.py
+++ app/errors.py
@@ -7,6 +7,11 @@ def not_found_error(error):
     return render_template('404.html'), 404
 
 
+@app.errorhandler(403)
+def forbidden(error):
+    return render_template('403.html'), 403
+
+
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
