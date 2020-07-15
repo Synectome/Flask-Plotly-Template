4ff139168cec5cbca67e1b494b51f61329e70f18
Synectome <jessedgb@gmail.com>
2020-07-15 15:22:02 -0400

    logging and .gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -5,7 +6,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_uploads import DATA, UploadSet
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,6 +18,7 @@ datafiles = UploadSet('datafiles', DATA)
 
 # miguel grinberg error logging
 if not app.debug: # only run when debugging is disabled
+    # Emailing file logger
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
@@ -31,6 +33,18 @@ if not app.debug: # only run when debugging is disabled
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+    # Rotating File Logger (not emailing)
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/FullSite.log', maxBytes=20480,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
 # This import causes errors if it is at the top of the file
 from app import routes, models, errors, privatefunctions
